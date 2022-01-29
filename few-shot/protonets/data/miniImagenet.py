17520c9d830b8f2e4eb52bc93250459889b4959d
csruiliu <rui.liu@proton.me>
2022-01-28 20:11:14 -0600

    prepare for miniImagenet

---
+++ few-shot/protonets/data/miniImagenet.py
@@ -0,0 +1,4 @@
+
+
+def load(opt, splits):
+    pass
--- few-shot/protonets/utils/data.py
+++ few-shot/protonets/utils/data.py
@@ -1,8 +1,11 @@
 import protonets.data
 
+
 def load(opt, splits):
     if opt['data.dataset'] == 'omniglot':
         ds = protonets.data.omniglot.load(opt, splits)
+    elif opt['data.dataset'] == 'miniImagenet':
+        ds = protonets.data.miniImagenet.load(opt, splits)
     else:
         raise ValueError("Unknown dataset: {:s}".format(opt['data.dataset']))
 
